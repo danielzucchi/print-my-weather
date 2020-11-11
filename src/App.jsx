276a447d6f443b0b7927e6fe175a96c0149a1203
Daniel Zucchi <danielzucchi@Daniels-MacBook-Pro.local>
2020-11-11 18:04:20 +0000

    Fetch weather button

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,25 @@ import getWeather from './weatherService';
 import './App.css';
 
 const App = () => {
+  const [location, setLocation] = useState({});
   const [weather, setWeather] = useState(null);
 
   useEffect(() => {
-    async function fetchWeather() {
-      const todayWeather = await getWeather({
-        lat: '-0.013',
-        lon: '51.49',
-        units: 'metric',
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
       });
+    });
+  }, [setLocation]);
 
-      setWeather(todayWeather);
-      console.log(todayWeather);
-    }
-
-    fetchWeather();
-  }, [setWeather]);
+  const fetchWeather = () => {
+    getWeather({
+      lat: location.lat,
+      lon: location.lon,
+      units: 'metric',
+    }).then((todayWeather) => setWeather(todayWeather));
+  };
 
   const icon = weather
     ? `http://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`
@@ -26,6 +29,9 @@ const App = () => {
 
   return (
     <div className="App">
+      <button type="button" onClick={() => fetchWeather()}>
+        Get weather
+      </button>
       <header className="App-header">
         {weather && (
           <>
