5c0b2c6e51cd3f1f4ae77b690d92b9ab395f45d8
Daniel Zucchi <danielzucchi@Daniels-MacBook-Pro.local>
2020-11-11 18:41:18 +0000

    Displayin weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,66 @@
-import React, { useEffect, useState } from 'react';
-import getWeather from './weatherService';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import getWeather from './services/weatherService'
 
 const App = () => {
-  const [location, setLocation] = useState({});
-  const [weather, setWeather] = useState(null);
+  const [location, setLocation] = useState({})
+  const [weather, setWeather] = useState(null)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       setLocation({
         lat: position.coords.latitude,
         lon: position.coords.longitude,
-      });
-    });
-  }, [setLocation]);
+      })
+    })
+  }, [setLocation])
 
   const fetchWeather = () => {
     getWeather({
       lat: location.lat,
       lon: location.lon,
       units: 'metric',
-    }).then((todayWeather) => setWeather(todayWeather));
-  };
+    })
+      .then((todayWeather) => setWeather(todayWeather))
+      .catch(() => console.log('Something went wrong.'))
+  }
 
-  const icon = weather
-    ? `http://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`
-    : null;
+  const AppWrapper = styled.div`
+    margin: 0 auto;
+    width: 700px;
+    background-color: #c2c2c2;
+  `
 
   return (
-    <div className="App">
+    <AppWrapper>
       <button type="button" onClick={() => fetchWeather()}>
         Get weather
       </button>
       <header className="App-header">
         {weather && (
           <>
-            <img src={icon} alt="weather" />
+            <img
+              src={`http://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`}
+              alt="weather"
+            />
+            <p>{Math.floor(weather.current.temp)}</p>
             <p>{weather.current.weather[0].description}</p>
+            {weather.daily.map((day) => (
+              <div key={day.dt}>
+                <img
+                  src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+                  alt="weather"
+                />
+                <p>{Math.floor(day.temp.day)}</p>
+                <p>{day.weather[0].description}</p>
+              </div>
+            ))}
           </>
         )}
       </header>
-    </div>
-  );
-};
+    </AppWrapper>
+  )
+}
 
-export default App;
+export default App
